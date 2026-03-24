d2e8e0d153077c4bee397525b2a149f38fc62ab1
RopeABN <vadimbark216@gmail.com>
2026-03-24 20:08:51 +0100

    The beginning

---
+++ sort_obs_replays.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/bash
+
+base="/run/media/small_torba/obs_vids"
+desktop=(
+    "foot" "Steam" "Discord" "Zen Browser" "Telegram" "OBS 32.0.4 - Profile: Untitled - Scenes: Untitled"
+)
+title=`hyprctl activewindow | rg -N --trim initialTitle: -r ''`
+title=$(printf "%s" "$title" | tr -cd '[:print:]')
+
+sleep 5
+
+for i in "${desktop[@]}"; do
+	if [[ $title == "$i" ]] ; then
+		ShGODesktop=1
+		# echo "$i exists" # for debug
+	else
+		:
+	fi
+done
+
+if [[ -n "$(ls -A "$base/unsorted")" && $ShGODesktop != 1 ]]; then
+    notify-send "Saving clip to: $title"
+elif [[ -n "$(ls -A "$base/unsorted")" && $ShGODesktop == 1 ]]; then
+    notify-send "Saving clip to: desktop"
+else
+    notify-send "Didn't save any file"
+fi
+
+if [[ $ShGODesktop != 1 ]]; then
+	mkdir -p "$base/$title"
+else
+	:
+fi
+
+if [[ -z $title || $ShGODesktop == 1 ]]; then
+	mv $base/unsorted/*.mp4 "$base/Desktop/"
+else
+	mv $base/unsorted/*.mp4 "$base/$title"
+fi
