4a1c1c1a3ef3fe00d7450c8c54a3369d7e0b1146
RopeABN <vadimbark216@gmail.com>
2026-03-25 16:46:15 +0100

    Making the script faster

--- hypr_sort_obs_replay.sh
+++ hypr_sort_obs_replay.sh
@@ -11,26 +11,39 @@ title=$(printf "%s" "$title" | tr -cd '[:print:]') # to cut every hidden char
 for i in "${desktop[@]}"; do
     if [[ $title == "$i" ]] ; then
         ShGODesktop=1
-        # echo "$i exists" # for debug
-    else
-        :
     fi
 done
 
 j=0
-while [[ -z "$(ls -A "$unsorted_base")" && $j != 10 ]]; do
-    ((j++))
-    sleep 1
-done
+# while [[ -z "$(ls -A "$unsorted_base")" && $j != 10 ]]; do
+#     ((j++))
+#     sleep 1
+# done
 
-if [[ $ShGODesktop != 1 ]]; then
+wait_to_notify () {
+    if [[ -z "$(ls -A "$unsorted_base")" && $j != 10 ]]; then
+        ((j++))
+        echo $j
+        sleep 1
+        wait_to_notify
+    elif [[ $j -ge 10 ]]; then
+        skip=1
+    fi
+}
+wait_to_notify
+
+if [[ $ShGODesktop != 1 && $skip != 1 ]]; then
     notify-send "Saving clip to: $title"
-elif [[ $ShGODesktop == 1 ]]; then
+elif [[ $ShGODesktop == 1 && $skip != 1 ]]; then
     notify-send "Saving clip to: desktop"
+else
+    notify-send "Didn't save any file"
 fi
 
 # wait for obs to save the clip. Just in case
-sleep 5
+echo "Waiting for obs"
+sleep 3
+echo "Stopping to wait for obs"
 
 if [[ $ShGODesktop != 1 ]]; then
     mkdir -p "$base/$title"
